32d443034464436c0d577af9ec124a66c37841cb
Vitor Stachanczyk <vitor.stachanczyk@gmail.com>
2020-05-27 15:55:47 -0300

    first commit

---
+++ src/main/java/com/lightninggames/store/LightninggamesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.lightninggames.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightninggamesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightninggamesStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lightninggames/LightninggamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightninggames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightninggamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
