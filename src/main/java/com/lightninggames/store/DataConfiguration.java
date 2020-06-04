7ae8908bbebae01a60037e58d16a987ced6d5ad6
Vitor Stachanczyk <vitor.stachanczyk@gmail.com>
2020-06-03 22:51:41 -0300

    update

--- src/main/java/com/lightninggames/store/DataConfiguration.java
+++ src/main/java/com/lightninggames/store/DataConfiguration.java
@@ -1,11 +1,7 @@
 package com.lightninggames.store;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -16,20 +12,25 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfiguration {
 	
-	  @Bean
-	    public BasicDataSource dataSource() throws URISyntaxException {
-	        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+	@Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/lightninggames?useTimezone=true&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
 
-	        String username = dbUri.getUserInfo().split(":")[0];
-	        String password = dbUri.getUserInfo().split(":")[1];
-	        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
-
-	        BasicDataSource basicDataSource = new BasicDataSource();
-	        basicDataSource.setUrl(dbUrl);
-	        basicDataSource.setUsername(username);
-	        basicDataSource.setPassword(password);
-
-	        return basicDataSource;
-	    }
 }
-
